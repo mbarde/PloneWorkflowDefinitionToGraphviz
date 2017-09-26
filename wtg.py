83917664202e5e0170ad8d5d48beac8405b51066
Matthias Barde <mbarde@uni-koblenz.de>
2017-09-26 13:24:29 +0200

    Initial commit

---
+++ wtg.py
@@ -0,0 +1,44 @@
+import sys
+import xml.etree.ElementTree
+from graphviz import Digraph
+
+if len(sys.argv) < 2:
+  sys.exit(0)
+
+xmlfile = sys.argv[1]
+
+root = xml.etree.ElementTree.parse(xmlfile).getroot()
+
+title = root.get('title')
+
+dot = Digraph(comment=title)
+
+transitions = dict()
+for trans in root.findall('transition'):
+  newTrans = dict()
+  newTrans['id'] = trans.get('transition_id')
+  newTrans['to'] = trans.get('new_state')
+  newTrans['label'] = trans.get('title')
+
+  guard_roles = []
+  for guard in trans.findall('guard'):
+    for guard_role in guard.findall('guard-role'):
+      guard_roles.append( guard_role.text )
+  if len(guard_roles) > 0:
+    newTrans['label'] += '\n(' + ',\n'.join(guard_roles) + ')'
+
+  transitions[newTrans['id']] = newTrans
+
+edges = []
+for state in root.findall('state'):
+  node_id = state.get('state_id')
+  dot.node(node_id, state.get('title'), shape='box')
+  for exTrans in state.findall('exit-transition'):
+    newEdge = transitions[ exTrans.get('transition_id') ]
+    newEdge['from'] = node_id
+    edges.append(newEdge)
+
+for edge in edges:
+  dot.edge(edge['from'], edge['to'], label=edge['label'])
+
+dot.render(title, view=True)
